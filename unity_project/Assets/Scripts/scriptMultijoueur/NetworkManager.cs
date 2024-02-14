e9b719debba79d34f0874ac438f2e33373be937f
Blackearth2 <perraudamaury@yahoo.com>
2024-02-14 01:52:33 +0100

    multijoueur button

--- unity_project/Assets/Scripts/scriptMultijoueur/NetworkManager.cs
+++ unity_project/Assets/Scripts/scriptMultijoueur/NetworkManager.cs
@@ -1,15 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class NetworkManager : MonoBehaviour
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class NetworkManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Connect();
+    }
+    public void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
     }
 
+    public void Multijoueur() => PhotonNetwork.JoinRandomRoom();
+
+    public override  void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to join a room and failed");
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4});
+    }
+
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("joined a rom");
+    }
     // Update is called once per frame
     void Update()
     {
