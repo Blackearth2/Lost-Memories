48646c90da1f166799212504456ff586c1737aeb
Blackearth2 <perraudamaury@yahoo.com>
2024-02-17 17:59:42 +0100

    maps

--- unity_project/Assets/Scripts/scriptMap/DugeonGenerator.cs
+++ unity_project/Assets/Scripts/scriptMap/DugeonGenerator.cs
@@ -1,26 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-
 public class DugeonGenerator
 {
-    RoomNode rootNode;
-    List<RoomNode> allSPaceNodes = new List<RoomNode>();  
+    
+    List<RoomNode> allNodesCollection = new List<RoomNode>();
     private int dungeonWidth;
     private int dungeonLength;
-    // Start is called before the first frame update
 
     public DugeonGenerator(int dungeonWidth, int dungeonLength)
     {
         this.dungeonWidth = dungeonWidth;
         this.dungeonLength = dungeonLength;
-
     }
 
 
-    internal object CalculateRooms(int maxIterations,int roomWidthMin,int roomLengthMax)
-    {
-        throw new NotImplementedException();
 
+    public List<Node> CalculateDungeon(int maxIterations, int roomWidthMin, int roomLengthMin, float roomBottomCornerModifier, float roomTopCornerMidifier, int roomOffset, int corridorWidth)
+    {
+        BinarySpacePartitioner bsp = new BinarySpacePartitioner(dungeonWidth, dungeonLength);
+        allNodesCollection = bsp.PrepareNodesCollection(maxIterations, roomWidthMin, roomLengthMin);
+        List<Node> roomSpaces = StructureHelper.TraverseGraphToExtractLowestLeafes(bsp.RootNode);
+
+        RoomGenerator roomGenerator = new RoomGenerator(maxIterations, roomLengthMin, roomWidthMin);
+        List<RoomNode> roomList = roomGenerator.GenerateRoomsInGivenSpaces(roomSpaces, roomBottomCornerModifier, roomTopCornerMidifier, roomOffset);
+
+        CorridorsGenerator corridorGenerator = new CorridorsGenerator();
+        var corridorList = corridorGenerator.CreateCorridor(allNodesCollection, corridorWidth);
+        
+        return new List<Node>(roomList).Concat(corridorList).ToList();
     }
 }
--- unity_project/Assets/Scripts/scriptMap/RoomNode.cs
+++ unity_project/Assets/Scripts/scriptMap/RoomNode.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RoomNode : MonoBehaviour
+﻿using UnityEngine;
+public class RoomNode : Node
 {
-    // Start is called before the first frame update
-    void Start()
+    public RoomNode(Vector2Int bottomLeftAreaCorner, Vector2Int topRightAreaCorner, Node parentNode, int index) : base(parentNode)
     {
-        
+        this.BottomLeftAreaCorner = bottomLeftAreaCorner;
+        this.TopRightAreaCorner = topRightAreaCorner;
+        this.BottomRightAreaCorner = new Vector2Int(topRightAreaCorner.x, bottomLeftAreaCorner.y);
+        this.TopLeftAreaCorner = new Vector2Int(bottomLeftAreaCorner.x, TopRightAreaCorner.y);
+        this.TreeLayerIndex = index;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int Width { get => (int)(TopRightAreaCorner.x - BottomLeftAreaCorner.x); }
+    public int Length { get => (int)(TopRightAreaCorner.y - BottomLeftAreaCorner.y); }
 }
