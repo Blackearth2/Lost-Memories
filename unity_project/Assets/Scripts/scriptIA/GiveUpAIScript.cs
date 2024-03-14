38350fbf468085d0028ba7bb02ac6e5322bdaac0
EdwinTilIllan <pierre-yang.touret@epita.fr>
2024-03-14 20:25:41 +0100

    AIScripts
    Ajout de deux scripts d'IA modulable avec un niveau d'aggresivite variant de 0 a 2

---
+++ unity_project/Assets/Scripts/scriptIA/GiveUpAIScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3349842481e5135ac110ece4254bc78b9fd9cb8028906044f22094a6385d55a2
+size 2296
---
+++ unity_project/Assets/Scripts/scriptPlayer/PlayerManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e42d7e54e93a6540fc76f696fc0a751f044385abd362bbbc72b7627c57c14c47
+size 315
