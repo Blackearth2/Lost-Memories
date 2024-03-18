3d0f58aaad66461f1842b8e15336a953e4c42cf9
Blackearth2 <perraudamaury@yahoo.com>
2024-03-18 11:19:44 +0100

    new menu

---
+++ unity_project/Assets/Scenes/MenusRename/MainMenu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c7100a1c0eff6941a909e4c0d58944388f41cbcb5fa01c38658d4c14cf41ac1
+size 818
--- unity_project/Assets/Scripts/scriptMenu/Menu.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:eac76f2aeef01038af389bfaa02780ed3344b1256267ebbad1534f215cd6690e
-size 359
